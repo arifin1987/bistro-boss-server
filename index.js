860a77498e2aabd0064dcb99a4c611fbf0ba39c5
Shohidul Arifin <shohidularifin@gmail.com>
2023-06-03 13:01:36 -0700

    send JWT token to backend for verificaion

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const verifyJWT = (req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'});
+  }
+  // bearer, token
+  const token= authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+
+    }
+    req.decoded= decoded;
+  })
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.59h68ks.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +46,13 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    
+    app.post('/jwt', (req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      res.send({token})
+    })
+    
     // users related apis
     app.get('/users',async(req,res)=>{
       const result = await usersCollection.find().toArray();
@@ -73,10 +96,15 @@ async function run() {
     })
 
     // cart collection
-    app.get('/carts', async(req,res)=>{
+    app.get('/carts',verifyJWT, async(req,res)=>{
       const email = req.query.email;
       if(!email){
         res.send([])
+      }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error:true,message:'forbidden access'})
       }
       const query={email:email};
       const result= await cartCollection.find(query).toArray();
