90de915f7964e8ef5157bdafd172be6e371b09af
Shohidul Arifin <shohidularifin@gmail.com>
2023-06-03 23:25:59 -0700

    check admin verifyJWT using email

--- index.js
+++ index.js
@@ -71,6 +71,20 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    // security layer: verifyJWT
+    // email same
+    // check admin
+    app.get('/users/admin/:email', verifyJWT,async(req,res)=>{
+        const email = req.params.email;
+        if(req.decoded.email !== email){
+          res.send({admin:false})
+        }
+        const query ={email:email}
+        const user = await usersCollection.findOne(query);
+        const result ={admin:user?.role === 'admin'}
+        res.send(result);
+    })
     
     app.patch('/users/admin/:id', async(req,res)=>{
       const id = req.params.id;
